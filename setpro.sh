0970d650456442450ad85dd56cae33bd1e5d4b07
xinloveye <992950082@qq.com>
2021-10-28 10:57:00 +0800

    123

--- setpro.sh
+++ setpro.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git config --global https.proxy https://172.18.4.23:7890
-git config --global http.proxy http://172.18.4.23:7890
-# git config --global http.proxy http://127.0.0.1:7890
-# git config --global https.proxy https://127.0.0.1:7890
+#git config --global https.proxy https://172.18.4.23:7890
+#git config --global http.proxy http://172.18.4.23:7890
+git config --global http.proxy http://127.0.0.1:7890
+git config --global https.proxy https://127.0.0.1:7890
